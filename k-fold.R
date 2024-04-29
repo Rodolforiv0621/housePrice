6ac93be7972165a6721f8d3496598c67e58026c6
Rodolforiv0621 <78060969+Rodolforiv0621@users.noreply.github.com>
2024-04-28 22:55:21 -0700

    k-fold

---
+++ k-fold.R
@@ -0,0 +1,18 @@
+install.packages("caret")
+library(caret)
+
+train_control <-  trainControl(
+  method = "cv",  # K-fold cross-validation
+  number = 10     # Number of folds
+)
+
+# Define the model 
+formula <- price ~ zip_code + num_bed + num_bath + home_area
+
+# Train the model
+model <- train(formula, data = data, method = "lm", trControl = train_control)
+
+print(model)
+
+summary(model)
+
